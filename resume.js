29832592b2767069738bf4d324ae6e392a4fa008
Cooper Yao <309318267@qq.com>
2017-09-20 15:39:20 +0800

    更新响应式效果

--- resume.js
+++ resume.js
@@ -15,33 +15,33 @@ $(function(){
     // 封面轮播动画
     $(window).scroll(function(){
         var scrollH = $(document).scrollTop();
-        var alist = $("nav").find("a");
-        var nowFloor = Math.floor((scrollH-600)/800);
+        var alist = $(".navbar").find("a");
+        var nowFloor = Math.floor((scrollH-500)/800);
         if(scrollH >= 800){
-            $("nav").css({
+            $(".navbar").css({
                 "position":"fixed",
                 "top":"0",
+                "width":"100%",
             });
         }else{
-            $("nav").css({
-                "position":"static",
-                "top":"800px",
+            $(".navbar").css({
+                "position":"relative",
             })
         }
         // 导航条触发fixed效果
-        if(scrollH <= 600){
-            $(alist).removeClass();
+        if(scrollH <= 500){
+            $(alist).removeClass("active");
         }else{
             $(alist).eq(nowFloor)
             .addClass("active").parent()
-            .siblings().children().removeClass();
+            .siblings().children().removeClass("active");
         }
         // 滚动条触发导航高亮
     })
-    $("nav").find("a").click(function(){
+    $(".navbar").find("a").click(function(){
         $(this).addClass("active")
         .parent().siblings()
-        .children().removeClass();
+        .children().removeClass("active");
     })
     //导航条当前位置效果
 })
