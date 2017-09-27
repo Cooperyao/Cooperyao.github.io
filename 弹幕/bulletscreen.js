72128ad4cf026a898842806aab462a9f04e28ec6
Cooper Yao <309318267@qq.com>
2017-09-27 15:57:42 +0800

    视频网

--- 弹幕/bulletscreen.js
+++ 弹幕/bulletscreen.js
@@ -1,12 +1,26 @@
 $(function () {
     $(".btn").click(function () {
-        text=$(".input_text").val();//获取文本
-        var top=Math.random()*$(".content").height();//随机高度
+        var text=$(".input_text").val();//获取文本
+        var blurH = $(".blur").height();
+        var top=Math.random()*(blurH/2);//随机高度
         $("<div class='float'>"+text+"</div>").css("top",top)
-            .appendTo(".content");//创建新的div并加入到content
+            .appendTo(".blur");//创建新的div并加入到blur
         $(".input_text").val("");//清空输入框
         $(".float").animate({left:"-400px"},13000,function () {
             $(this).remove();//超出范围删除该div
         });
     })
+    // 弹幕效果
+    $(".media a").click(function(){
+        var link = $(this).data("src");
+        var txt = $(this).text();
+        console.log(txt);
+        $(".content").attr("src",link);
+        $(".content")[0].play();
+        $(this).children(".media-body").addClass("active")
+        .closest(".media").siblings().find(".media-body").removeClass("active");
+        $(".mtitle small").text(txt);
+    })
+    //点播视频
+
 })
